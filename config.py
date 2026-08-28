d9aee7e46d2a0a81f89fee72c69ec955c3db01c5
Zalinn12 <189938948+MySelfZalin@users.noreply.github.com>
2026-08-28 16:09:48 +0300

    fix: update selectors and login marker after site redesign

--- config.py
+++ config.py
@@ -1,26 +1,27 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
     )
-    
+
     ICAN_MAIL: str
     ICAN_PASSWORD: str
     MIN_SECONDS_INTERVAL: int
     MAX_SECONDS_INTERVAL: int
     HEADLESS: bool
-    SEL_LOGGED_IN_MARKER: str
-    
+
     LOGIN_URL: str = "https://rus.icancgm.com/review/login?redirect=https://rus.icancgm.com/review/"
     DEVICE_PAGE_URL: str = "https://rus.icancgm.com/review/workbench"
     SEL_LOGIN_INPUT: str = "input[name='account']"
     SEL_PASS_INPUT: str = "input[name='password']"
     SEL_SUBMIT_BTN: str = "button:has-text('Login')"
-    SEL_VALUE: str = r"span.text-\[36px\]"
-    SEL_UNIT: str = "span:has-text('mmol/L')"
-    SEL_TIME: str = "p.whitespace-nowrap"
+    SEL_LOGGED_IN_MARKER: str = ".layout-team"
+    SEL_VALUE: str = "span.text-\\[36px\\]"
+    SEL_UNIT: str = "span.text-\\[14px\\]:has-text('mmol/L')"
+    SEL_TIME: str = "p.whitespace-nowrap.text-\\[14px\\]"
 
 
-config = Config()    
+config = Config()
